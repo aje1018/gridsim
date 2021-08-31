467aaee4ac42dd7e864c1cd058287a7a1ebd92cb
aje1018 <aje1018@wildcats.unh.edu>
2021-08-31 19:57:36 -0400

    Milestone 1 commit

--- app/src/main/java/com/example/gridsim/MainActivity.java
+++ app/src/main/java/com/example/gridsim/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.gridsim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button but1;
+    private Button but2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        but1 = (Button) findViewById(R.id.buttonLeft);
+        but2 = (Button) findViewById(R.id.buttonRight);
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.popup1,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.popup2,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
